04795d83d761e6310d0e422980b92d3ac23dee19
Xterminal1 <124107188+Xterminal1@users.noreply.github.com>
2025-03-21 17:12:59 -0700

    Update rangesOptimal.cpp

--- cpp/rangesOptimal.cpp
+++ cpp/rangesOptimal.cpp
@@ -31,34 +31,34 @@ int main() {
 
     auto start = std::chrono::high_resolution_clock::now();
 
-    unsigned long long limit = 10'000'000'000;
+    unsigned long long limit = 1'000'000;
     unsigned long long r = limit / 10; // CPU has 12 cores
 
-    // for (unsigned long long n = 1; n <= limit; n += 2) {
-    //     collatz(n);
-    // }
+    for (unsigned long long n = 1; n <= limit; n += 2) {
+        collatz(n);
+    }
     
-    std::thread t1(calc, 1, r);
-    std::thread t2(calc, r + 1, r * 2);
-    std::thread t3(calc, 2 * r + 1, r * 3);
-    std::thread t4(calc, 3 * r + 1, r * 4);
-    std::thread t5(calc, 4 * r + 1, r * 5);
-    std::thread t6(calc, 5 * r + 1, r * 6);
-    std::thread t7(calc, 6 * r + 1, r * 7);
-    std::thread t8(calc, 7 * r + 1, r * 8);
-    std::thread t9(calc, 8 * r + 1, r * 9);
-    std::thread t10(calc, 9 * r + 1, r * 10);
+    // std::thread t1(calc, 1, r);
+    // std::thread t2(calc, r + 1, r * 2);
+    // std::thread t3(calc, 2 * r + 1, r * 3);
+    // std::thread t4(calc, 3 * r + 1, r * 4);
+    // std::thread t5(calc, 4 * r + 1, r * 5);
+    // std::thread t6(calc, 5 * r + 1, r * 6);
+    // std::thread t7(calc, 6 * r + 1, r * 7);
+    // std::thread t8(calc, 7 * r + 1, r * 8);
+    // std::thread t9(calc, 8 * r + 1, r * 9);
+    // std::thread t10(calc, 9 * r + 1, r * 10);
 
-    t1.join();
-    t2.join();
-    t3.join();
-    t4.join();
-    t5.join();
-    t6.join();
-    t7.join();
-    t8.join();
-    t9.join();
-    t10.join();
+    // t1.join();
+    // t2.join();
+    // t3.join();
+    // t4.join();
+    // t5.join();
+    // t6.join();
+    // t7.join();
+    // t8.join();
+    // t9.join();
+    // t10.join();
 
     auto end = std::chrono::high_resolution_clock::now();
     auto timeNS = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
@@ -66,6 +66,3 @@ int main() {
     std::cout << "Time taken: " << timeNS.count() << " ns | " << timeNS.count() / 1e9 << " s";
     return 0;
 }
-/*
-& 'c:\Users\Sathv\.vscode\extensions\ms-vscode.cpptools-1.23.6-win32-x64\debugAdapters\bin\WindowsDebugLauncher.exe' '--stdin=Microsoft-MIEngine-In-rddzrxmx.fvf' '--stdout=Microsoft-MIEngine-Out-t0abfvls.aoz' '--stderr=Microsoft-MIEngine-Error-wgn2p5nj.3g0' '--pid=Microsoft-MIEngine-Pid-u5axodzq.25o''--dbgExe=C:\msys64\ucrt64\bin\gdb.exe' '--interpreter=mi'
-*/
